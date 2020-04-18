c6439d5532a60250cc631d2a49581311131abbbd
MOHSIN <MOHSIN@DESKTOP-9JAHD12>
2020-04-18 15:12:25 +0530

    intial commot with config of property file and seting up db

---
+++ onlinebookApi/src/main/java/com/bookApi/onlinebookApi/OnlinebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookApi.onlinebookApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookApiApplication.class, args);
+	}
+
+}
---
+++ onlinebookApi/src/test/java/com/bookApi/onlinebookApi/OnlinebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookApi.onlinebookApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
